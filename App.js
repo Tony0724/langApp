25e54eab9216a1bcee67ce48fe7fa43c85f49889
Tony0724 <0724ksm@naver.com>
2023-07-09 16:06:27 +0900

    used interpolate to be more professional animation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components/native';
-import { Animated, Easing, TouchableOpacity } from 'react-native';
+import { Animated, Pressable } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
@@ -16,22 +16,32 @@ const AnimatedBox = Animated.createAnimatedComponent(Box)
 
 export default function App() {
   const [up, setUp] = useState(false);
-  const Y = useRef(new Animated.Value(0)).current;
+  const Y_POSITION = useRef(new Animated.Value(300)).current;
   const toggleUp = () => setUp(prev => !prev);
   const moveUp = () => {
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 300 : -300,
+      duration: 1000,
       useNativeDriver: true,
-      easing: Easing.cubic
     }).start(toggleUp)
   }
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.5, 1]
+  })
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0]
+  })
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox style={{
-          transform: [{translateY: Y}]
+          borderRadius,
+          opacity,
+          transform: [{translateY: Y_POSITION}]
         }} />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   )
 }
